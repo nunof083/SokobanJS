a2ef32ce497091aedeb0cf21e4816e99156e5f4b
Nuno Ferreira <nunof083@gmail.com>
2023-12-04 04:38:19 +0100

    moves

--- js/sokoban.js
+++ js/sokoban.js
@@ -4,7 +4,10 @@ var c = document.getElementById("mycanvas");
 var ctx = c.getContext("2d");
 var posX = 0;
 var posY = 0;
+var playerState = '0';
     
+
+// map
 var mapArray = 
 [   
     ["0", "0", "1", "1", "1", "1", "1", "0",],
@@ -13,10 +16,11 @@ var mapArray =
     ["1", "1", "1", "0", "3", "2", "1", "0",],
     ["1", "2", "1", "1", "3", "0", "1", "0",],
     ["1", "0", "1", "0", "2", "0", "1", "1",],
-    ["1", "3", "0", "4", "3", '3', "2", "1",],
+    ["1", "3", "0", "3", "3", '3', "2", "1",],
     ["1", "0", "0", "0", "2", "0", "0", "1",],
     ["1", "1", "1", "1", "1", "1", "1", "1",]
 ];
+
 
 // // * is where the character is
 // // 0 is for where we can move and is not the point of the box
@@ -25,15 +29,22 @@ var mapArray =
 // // 3 is where a box is
 // // 4 is when a box is at the point 
 
-function makeMap ()
-{
+var EMPTY = "0";
+var WALL = "1";
+var VOID = "2";
+var BOX = "3";
+var boxOnPoint = "4";
+var PLAYER = "*"
+
+//function that makes the map
+function makeMap() {
     ctx.clearRect(0, 0, c.width, c.height);
     var yellow = "#FFFF00";
     var gray = "#808080";
     var orange = "#FFA500";
-    var black = "#000000"
     posX = 0;
     posY = 0;
+
     for (var i = 0; i < mapArray.length; i++) {
         for (var j = 0; j < mapArray[i].length; j++) {
             if (mapArray[i][j] == '*') {
@@ -47,7 +58,7 @@ function makeMap ()
             } else if (mapArray[i][j] === '3') {
                 drawImage('./images/box.png', posX, posY);
             } else if (mapArray[i][j] === '4') {
-                makeSquare(posX, posY, black)
+                drawImage('./images/boxonpoint.png', posX, posY);
             }
             posX += 32;
         }
@@ -56,6 +67,7 @@ function makeMap ()
     }
 }
 
+//function to put images
 function drawImage(imageUrl, x, y) {
     var img = new Image();
     img.onload = function() {
@@ -64,6 +76,7 @@ function drawImage(imageUrl, x, y) {
     img.src = imageUrl;
 }
 
+//function to draw squares
 function makeSquare(posX, posY, colour) {
     ctx.beginPath();
     ctx.rect(posX, posY, 32, 32);
@@ -77,112 +90,138 @@ function makeSquare(posX, posY, colour) {
 function findPlayerCords() {
     const y = mapArray.findIndex((row) => row.includes('*'));
     const x = mapArray[y].indexOf('*');
+    
+    const above = mapArray[y - 1]?.[x];
+    const above2 = mapArray[y - 2]?.[x];
+    const below = mapArray[y + 1]?.[x];
+    const below2 = mapArray[y + 2]?.[x];
+    const sideLeft = mapArray[y]?.[x - 1];
+    const sideLeft2 = mapArray[y]?.[x - 2];
+    const sideRight = mapArray[y]?.[x + 1];
+    const sideRight2 = mapArray[y]?.[x + 2];
 
     return {
       x,
       y,
-      above: mapArray[y - 1][x],
-      below: mapArray[y + 1][x],
-      sideLeft: mapArray[y][x - 1],
-      sideRight: mapArray[y][x + 1],
+      above,
+      above2,
+      below,
+      below2,
+      sideLeft,
+      sideLeft2,
+      sideRight,
+      sideRight2
     };
 }
 
+//function to end the game
+function isGameOver() {
+    for (var i = 0; i < mapArray.length; i++) {
+        for (var j = 0; j < mapArray[i].length; j++) {
+            if (mapArray[i][j] === VOID) {
+                if (!hasBoxAtPoint(i, j)) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+function hasBoxAtPoint(row, col) {
+    return mapArray[row][col] === BOX || mapArray[row][col] === boxOnPoint;
+}
+
+//all moves of player
 function keys(e) {
     var playerCords = findPlayerCords();
     var playerCordsX = playerCords.x;
     var playerCordsY = playerCords.y;
     var boxMoved = false;
+    var direction;
     
     if ((e.keyCode == 87 || e.keyCode == 38) && (playerCords.above !== "1")) {
-        var nextCellabove = mapArray[playerCordsY - 1][playerCordsX];
-        if (nextCellabove === '3') {
-            var nextNextCellabove = mapArray[playerCordsY - 2][playerCordsX];
+        var nextCellAbove = mapArray[playerCordsY - 1][playerCordsX];
+        direction = "up";
+        if (nextCellAbove === '3') {
+            var nextNextCellAbove = mapArray[playerCordsY - 2][playerCordsX];
 
-            if (nextNextCellabove === '0' || nextNextCellAbove === '2') {
+            if (nextNextCellAbove === '0' || nextNextCellAbove === '2' || nextNextCellAbove === '4') {
                 mapArray[playerCordsY - 2][playerCordsX] = '3';
                 mapArray[playerCordsY - 1][playerCordsX] = '*';
                 boxMoved = true;
+            } else if (nextNextCellAbove === '1') {
+                boxMoved = false;
             }
         } else {
             mapArray[playerCordsY - 1][playerCordsX] = '*';
             boxMoved = true;
         }
+    } else if ((e.keyCode == 65 || e.keyCode == 37) && (playerCords.sideLeft !== "1")) {
+        var nextCellLeft = mapArray[playerCordsY][playerCordsX - 1];
+        direction = "left";
+        if (nextCellLeft === '3') {
+            var nextNextCellLeft = mapArray[playerCordsY][playerCordsX - 2];
 
-        if (boxMoved) {
-            mapArray[playerCordsY][playerCordsX] = '0';
-            makeMap();
-        }
-        mapArray[playerCordsY][playerCordsX] = '0';
-        makeMap();
-    }
-    if ((e.keyCode == 65 || e.keyCode == 37) && (playerCords.sideLeft !== "1")) {
-        var nextCellabove = mapArray[playerCordsY][playerCordsX - 1];
-        if (nextCellabove === '3') {
-            var nextNextCellabove = mapArray[playerCordsY][playerCordsX - 2];
-
-            if (nextNextCellabove === '0' || nextNextCellAbove === '2') {
+            if (nextNextCellLeft === '0' || nextNextCellLeft === '2' || nextNextCellLeft === '4') {
                 mapArray[playerCordsY][playerCordsX - 2] = '3';
                 mapArray[playerCordsY][playerCordsX - 1] = '*';
                 boxMoved = true;
+            } else if (nextNextCellLeft === '1') {
+                boxMoved = false;
             }
         } else {
             mapArray[playerCordsY][playerCordsX - 1] = '*';
             boxMoved = true;
         }
+    } else if ((e.keyCode == 83 || e.keyCode == 40) && (playerCords.below !== "1")) {
+        var nextCellBelow = mapArray[playerCordsY + 1][playerCordsX];
+        direction = "down";
+        if (nextCellBelow === '3') {
+            var nextNextCellBelow = mapArray[playerCordsY + 2][playerCordsX];
 
-        if (boxMoved) {
-            mapArray[playerCordsY][playerCordsX] = '0';
-            makeMap();
-        }
-        mapArray[playerCordsY][playerCordsX] = '0';
-        makeMap();
-    }
-    if ((e.keyCode == 83 || e.keyCode == 40) && (playerCords.below !== "1")) {
-        var nextCellabove = mapArray[playerCordsY + 1][playerCordsX];
-        if (nextCellabove === '3') {
-            var nextNextCellabove = mapArray[playerCordsY + 2][playerCordsX];
-
-            if (nextNextCellabove === '0' || nextNextCellAbove === '2') {
+            if (nextNextCellBelow === '0' || nextNextCellBelow === '2' || nextNextCellBelow === '4') {
                 mapArray[playerCordsY + 2][playerCordsX] = '3';
                 mapArray[playerCordsY + 1][playerCordsX] = '*';
                 boxMoved = true;
+            } else if (nextNextCellBelow === '1') {
+                boxMoved = false;
             }
         } else {
             mapArray[playerCordsY + 1][playerCordsX] = '*';
             boxMoved = true;
         }
+    } else if ((e.keyCode == 68 || e.keyCode == 39) && (playerCords.sideRight !== "1")) {
+        var nextCellRight = mapArray[playerCordsY][playerCordsX + 1];
+        direction = "right";
+        if (nextCellRight === '3') {
+            var nextNextCellRight = mapArray[playerCordsY][playerCordsX + 2];
 
-        if (boxMoved) {
-            mapArray[playerCordsY][playerCordsX] = '0';
-            makeMap();
-        }
-        mapArray[playerCordsY][playerCordsX] = '0';
-        makeMap();
-    }
-    if ((e.keyCode == 68 || e.keyCode == 39) && (playerCords.sideRight !== "1")) {
-        var nextCellabove = mapArray[playerCordsY][playerCordsX + 1];
-        if (nextCellabove === '3') {
-            var nextNextCellabove = mapArray[playerCordsY][playerCordsX + 2];
-
-            if (nextNextCellabove === '0' || nextNextCellAbove === '2' || nextNextCellAbove === '4') {
+            if (nextNextCellRight === '0' || nextNextCellRight === '2' || nextNextCellRight === '4') {
                 mapArray[playerCordsY][playerCordsX + 2] = '3';
                 mapArray[playerCordsY][playerCordsX + 1] = '*';
                 boxMoved = true;
+            } else if (nextNextCellRight === '1') {
+                boxMoved = false;
             }
         } else {
             mapArray[playerCordsY][playerCordsX + 1] = '*';
             boxMoved = true;
         }
+    }
 
-        if (boxMoved) {
-            mapArray[playerCordsY][playerCordsX] = '0';
-            makeMap();
-        }
+    if (boxMoved) {
+        console.log("Box moved");
+        
         mapArray[playerCordsY][playerCordsX] = '0';
+
+        if (isGameOver()) {
+            alert("Congratulations! You completed the level.");
+        }
+        
         makeMap();
     }
 }
 
 document.onkeydown = keys;
-makeMap ();
+makeMap();
